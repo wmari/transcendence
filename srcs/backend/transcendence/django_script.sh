1ae4696e28590d9bd7dac533a668f09667fb2881
wmari <wmari@myVM.myguest.virtualbox.org>
2025-01-23 12:30:19 +0100

    creation d'un superuser

---
+++ srcs/backend/transcendence/django_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Collect static files
+echo "Collect static files"
+python ./transcendence/manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python ./transcendence/manage.py migrate
+
+python ./transcendence/manage.py createsuperuser --username=admin --email=admin@example.com --noinput
+
+# Start server
+echo "Starting server"
+python ./transcendence/manage.py runserver 0.0.0.0:8000
