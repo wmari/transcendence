ca235c9692bcde0e869cccedebebc2249789bb5d
schai3001 <naver1@hotmail.fr>
2025-02-16 21:15:26 +0100

    commentaire friend

--- srcs/backend/transcendence/transcendence_site/views.py
+++ srcs/backend/transcendence/transcendence_site/views.py
@@ -38,33 +38,33 @@ def get_user(request):
     user = request.user
     return Response({"username": user.username, "email": user.email})
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def add_friend(request, friend_id):
-    user = request.user
-    try:
-        friend = MyUser.objects.get(id=friend_id)
-        if friend == user:
-            return Response({"error" : "Vous ne pouvez pas vous ajouter vous-même"}, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# def add_friend(request, friend_id):
+#     user = request.user
+#     try:
+#         friend = MyUser.objects.get(id=friend_id)
+#         if friend == user:
+#             return Response({"error" : "Vous ne pouvez pas vous ajouter vous-même"}, status=status.HTTP_400_BAD_REQUEST)
         
-        user.friends.add(friend)
-        return Response({"message": "Ami ajouté avec succès."}, status=status.HTTP_200_OK)
+#         user.friends.add(friend)
+#         return Response({"message": "Ami ajouté avec succès."}, status=status.HTTP_200_OK)
     
-    except MyUser.DoesNotExist:
-        return Response({"error": "Utilisateur introuvable."}, status=status.HTTP_404_NOT_FOUND)
+#     except MyUser.DoesNotExist:
+#         return Response({"error": "Utilisateur introuvable."}, status=status.HTTP_404_NOT_FOUND)
     
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def remove_friend(request, friend_id):
-    user = request.user
-    try:
-        friend = MyUser.objects.get(id=friend_id)
-        user.friends.remove(friend)
-        return Response({"message": "Ami supprimé avec succès."}, status=status.HTTP_200_OK)
+# @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# def remove_friend(request, friend_id):
+#     user = request.user
+#     try:
+#         friend = MyUser.objects.get(id=friend_id)
+#         user.friends.remove(friend)
+#         return Response({"message": "Ami supprimé avec succès."}, status=status.HTTP_200_OK)
 
-    except MyUser.DoesNotExist:
-        return Response({"error": "Utilisateur introuvable."}, status=status.HTTP_404_NOT_FOUND)
+#     except MyUser.DoesNotExist:
+#         return Response({"error": "Utilisateur introuvable."}, status=status.HTTP_404_NOT_FOUND)
 
 
 
