2e9dcc4f99a6f8879b327ea92bb5829c2a2b3269
Sann CHAI <sannchaileochai@gmail.com>
2025-02-22 11:11:14 +0100

    modif setting

--- srcs/backend/transcendence/mysite/settings.py
+++ srcs/backend/transcendence/mysite/settings.py
@@ -29,8 +29,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER='noreply.4.2.1.2.3@gmail.com'
-EMAIL_HOST_PASSWORD='qyeb ryfn xqfm gqgx'
+EMAIL_HOST_USER=os.environ.get("EMAIL")
+EMAIL_HOST_PASSWORD=os.environ.get("EMAIL_PASSWORD")
 
 
 
